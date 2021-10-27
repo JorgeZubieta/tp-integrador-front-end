510594494a912ccfb194f3710d36f4e9eb6609e0
jorge zubieta <zubietajor@gmail.com>
2021-10-27 12:34:38 -0300

    js terminado, html descuento corregido

--- index.js
+++ index.js
@@ -5,25 +5,24 @@ $(function () {
 
 // Calculo de valor de tickets
 function mat(){
-    var cat=document.getElementById("categoria");
-    var valorseleccionado=parseFloat(cat.options[cat.selectedIndex].value);
-    var cant=document.getElementById("cantidad").value;
+    var categoria=document.getElementById("categoria");
+    var valorseleccionado=parseFloat(categoria.options[categoria.selectedIndex].value);
+    var cantidad=document.getElementById("cantidad").value;
     var total=0;
 
     switch(valorseleccionado){
         case 1:
-            total=(cant*200)-( ((cant*200)*80)/100 );
+            total=cantidad*200-cantidad*200*80/100;
             // document.getElementById("total").value=total;
             break;
         case 2:
-            total=(cant*200)-( ((cant*200)*50)/100 );
+            total=cantidad*200-cantidad*200*50/100;
             // document.getElementById("total").value=total;
             break;
         case 3:
-            total=(cant*200)-( ((cant*200)*15)/100 );
+            total=cantidad*200-cantidad*200*15/100;
             // document.getElementById("total").value=total;
             break;
     }
     document.getElementById("total").value="  Total a Pagar: $ " + total;
-
 }
